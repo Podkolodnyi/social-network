66556d767ed81022a587a8da6d56684759a4674d
podkolodnyi <podkolodnyi@gmail.com>
2022-01-18 22:28:56 +0300

    action creator

--- src/components/layout/outlet/messages/messages.jsx
+++ src/components/layout/outlet/messages/messages.jsx
@@ -1,19 +1,45 @@
 import classes from "./messages.module.css";
 import Dialog from "./dialog/dialog";
 import Message from "./message/message";
+import React from "react";
+import {
+  addNewMessageActionCreator,
+  changeMessageTextActionCreator,
+} from "../../../state/state";
 
 let Messages = (props) => {
-  let dialog = props.messagesPage.dialogs.map((elem) => (
-    <Dialog key={elem.id} id={elem.id} name={elem.name} />
-  ));
-  let message = props.messagesPage.messages.map((elem) => (
-    <Message key={elem.id} id={elem.id} message={elem.message} />
-  ));
+  let messageText = React.createRef();
+
+  let onChangeText = () => {
+    props.dispatch(changeMessageTextActionCreator(messageText.current.value));
+  };
+
+  let onClickButton = () => {
+    props.dispatch(addNewMessageActionCreator());
+  };
 
   return (
     <div className={classes.container}>
-      <div className={classes.dialog}>{dialog}</div>
-      <div className={classes.message}>{message}</div>
+      <div className={classes.messages}>
+        <div className={classes.dialog}>
+          {props.messagesPage.dialogs.map((elem) => (
+            <Dialog key={elem.id} id={elem.id} name={elem.name} />
+          ))}
+        </div>
+        <div className={classes.message}>
+          {props.messagesPage.messages.map((elem) => (
+            <Message key={elem.id} id={elem.id} message={elem.message} />
+          ))}
+        </div>
+      </div>
+      <div className={classes.input}>
+        <input
+          ref={messageText}
+          onChange={onChangeText}
+          value={props.messagesPage.inputMessageText}
+        />
+        <button onClick={onClickButton}>new message</button>
+      </div>
     </div>
   );
 };
--- src/components/state/state.jsx
+++ src/components/state/state.jsx
@@ -1,3 +1,8 @@
+const CHANGE_MESSAGE_TEXT = "CHANGE_MESSAGE_TEXT";
+const ADD_NEW_MESSAGE = "ADD_NEW_MESSAGE";
+const ADD_NEW_POST = "ADD_NEW_POST";
+const CHANGE_POST_TEXT = "CHANGE_POST_TEXT";
+
 let store = {
   _state: {
     profilePage: {
@@ -16,6 +21,7 @@ let store = {
         { id: 1, message: "hello" },
         { id: 2, message: "hi" },
       ],
+      inputMessageText: "",
     },
   },
   _callSubscriber() {},
@@ -28,10 +34,10 @@ let store = {
   },
 
   dispatch(action) {
-    if (action.type === "CHANGE_POST_TEXT") {
+    if (action.type === CHANGE_POST_TEXT) {
       this._state.profilePage.inputPostText = action.text;
       this._callSubscriber();
-    } else if (action.type === "ADD_NEW_POST") {
+    } else if (action.type === ADD_NEW_POST) {
       this._state.profilePage.posts.push({
         id: this._state.profilePage.posts.length + 1,
         name: "user",
@@ -40,7 +46,38 @@ let store = {
       });
       this._state.profilePage.inputPostText = "";
       this._callSubscriber();
+    } else if (action.type === CHANGE_MESSAGE_TEXT) {
+      this._state.messagesPage.inputMessageText = action.text;
+      this._callSubscriber();
+    } else if (action.type === ADD_NEW_MESSAGE) {
+      this._state.messagesPage.messages.push({
+        id: this._state.messagesPage.messages.length + 1,
+        message: this._state.messagesPage.inputMessageText,
+      });
+      this._state.messagesPage.dialogs.push({
+        id: this._state.messagesPage.dialogs.length + 1,
+        name: "augustin",
+      });
+      this._state.messagesPage.inputMessageText = "";
+      this._callSubscriber();
     }
   },
 };
+
+export const changeMessageTextActionCreator = (text) => {
+  return { type: CHANGE_MESSAGE_TEXT, text: text };
+};
+
+export const addNewMessageActionCreator = () => {
+  return { type: ADD_NEW_MESSAGE };
+};
+
+export const changePostTextActionCreator = (text) => {
+  return { type: CHANGE_POST_TEXT, text: text };
+};
+
+export const addNewPostTextActionCreator = () => {
+  return { type: ADD_NEW_POST };
+};
+
 export default store;
